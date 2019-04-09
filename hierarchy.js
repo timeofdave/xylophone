d9036a530a3f56378f96fe2f877ba0c2514cd16c
David <timeofdave@gmail.com>
2019-04-09 00:44:32 -0500

    Added keystone generator
    'keystone' means the selected brick. 
    Also improved the sample hierarchy.

--- hierarchy.js
+++ hierarchy.js
@@ -25,17 +25,38 @@ var dom = { 'body': null };
 var selectedBrickDOM = null;
 
 function clickLogo() {
-    var elem1 = new Element('myid1', 'div', ['brick', 'level1'], [], [])
 
-    var elem21 = new Element('myid21', 'div', ['brick', 'level2'], [], [], true)
-    var elem22 = new Element('myid22', 'div', ['brick', 'level2'], [], [])
-    var elem31 = new Element('myid31', 'div', ['brick', 'level3'], [], [])
+    var elem0 = new Element('myid0', 'div', ['brick', 'level0'], [], [])
 
-    elem1.children.push(elem21);
-    elem1.children.push(elem22);
-    elem22.children.push(elem31);
+    var elem1 = new Element('myid12365', 'div', ['brick', 'level1'], [], [])
 
-    dom.body = elem1;
+    var elem11 = new Element('myid213647', 'div', ['brick', 'level2'], [], [])
+    var elem12 = new Element('myid225748', 'div', ['brick', 'level2'], [], [])
+    var elem121 = new Element('myid31262', 'div', ['brick', 'level3'], [], [])
+
+    var elem2 = new Element('myid125', 'div', ['brick', 'level1'], [], [])
+
+    var elem21 = new Element('myid21457', 'div', ['brick', 'level2'], [], [])
+    var elem211 = new Element('myid31215', 'div', ['brick', 'level3'], [], [])
+    var elem212 = new Element('myid313567', 'div', ['brick', 'level3'], [], [])
+    var elem22 = new Element('myid22659', 'div', ['brick', 'level2'], [], [])
+    var elem221 = new Element('myid31354', 'div', ['brick', 'level3'], [], [])
+
+    elem0.children.push(elem1);
+    elem0.children.push(elem2);
+
+    elem1.children.push(elem11);
+    elem1.children.push(elem12);
+    elem12.children.push(elem121);
+
+    elem2.children.push(elem21);
+    elem2.children.push(elem22);
+
+    elem21.children.push(elem211);
+    elem21.children.push(elem212);
+    elem22.children.push(elem221);
+
+    dom.body = elem0;
 
     updateAll();
 }
@@ -64,7 +85,7 @@ function updateReal() {
 
     // recreate the hierarchy from state
     var hierarchy = document.getElementById("hierarchy");
-    createHierarchy(dom.body, hierarchy, 1);
+    createHierarchy(dom.body, hierarchy, 0);
 }
 
 function createHierarchy(root, hierarchy, level) {
@@ -96,26 +117,57 @@ function createHierarchy(root, hierarchy, level) {
 }
 
 function createKeystone(obj, level) {
-    var elem = document.createElement('div');
-    elem.setAttribute('id', obj.id);
+    var keystone = document.createElement('div');
+    keystone.setAttribute('id', obj.id);
     var classes = 'brick';
     classes += ' level' + level;
-    classes += ' keystone'
-    elem.setAttribute('class', classes);
-    elem.innerHTML = `
-        ID<input id="id" value="brick3"/>
-        Tag<input id="tag" value="div"/><br /><br />
-        Classes<br />
-        <input id="class1" value="class1"/> X<br />
-        Properties<br />
-        <input id="prop1" value="background-color: #ffffff"/> X &nbsp;!<br />
-        <input id="prop1" value="background-color: #ffffff"/> X <br />
-        <input id="prop1" value="background-color: #ffffff"/> X <br />
-    `;
+    classes += ' keystone';
+    keystone.setAttribute('class', classes);
 
-    hierarchy.appendChild(elem); // Add to the DOM
+    addText(keystone, 'ID');
+    addInput(keystone, obj.id, true);
+    addText(keystone, 'Tag');
+    addInput(keystone, obj.tag, true);
+    addLineBreak(keystone);
 
-    return elem;
+    addLineBreak(keystone);
+    addText(keystone, 'Classes');
+    addLineBreak(keystone);
+    for (var i = 0; i < obj.classes.length; i++) {
+        addInput(keystone, obj.classes[i]);
+        addLineBreak(keystone);
+    }
+
+    addLineBreak(keystone);
+    addText(keystone, 'Properties');
+    addLineBreak(keystone);
+    for (var i = 0; i < obj.props.length; i++) {
+        addInput(keystone, obj.props[i]);
+        addLineBreak(keystone);
+    }
+    
+
+
+    return keystone;
+}
+
+function addText(parent, text) {
+    var elem = document.createTextNode(text);
+    parent.appendChild(elem);
+}
+
+function addLineBreak(parent) {
+    var elem = document.createElement('br');
+    parent.appendChild(elem);
+}
+
+function addInput(parent, value, short = false) {
+    var elem = document.createElement('input');
+    if (short) {
+        elem.setAttribute('class', 'short');
+    }
+    elem.setAttribute('value', value);
+    parent.appendChild(elem);
 }
 
 
